f0ef492bc63efc9df5c60e784ce477b5c5440cb8
João Arias <joao.p.arias@gmail.com>
2022-08-17 03:22:04 -0300

    Backend project recreated

--- backend/src/main/java/com/devsuperior/dscatalog/DsCatalogDevSuperiorApplication.java
+++ backend/src/main/java/com/devsuperior/dscatalog/DsCatalogDevSuperiorApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DscatalogApplication {
+public class DsCatalogDevSuperiorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DscatalogApplication.class, args);
+		SpringApplication.run(DsCatalogDevSuperiorApplication.class, args);
 	}
 
 }
